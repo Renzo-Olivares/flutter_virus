1f60e113e26d20d324e6afeb260a5875a8022266
Renzo <rmolivares@renzo-olivares.dev>
2020-06-27 10:13:09 -0700

    Split main into app and home

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fluttervirus/main.dart';
+import 'package:fluttervirus/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
