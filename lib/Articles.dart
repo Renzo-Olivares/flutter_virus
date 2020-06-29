24f1570ca980754e1894b763d59165b2e429660f
bryan <bryan.olivarez14@gmail.com>
2020-06-28 17:02:56 -0700

    Use webview to use the actual websites for the app. Also, implemented logos for each news source.

--- lib/Articles.dart
+++ lib/Articles.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'Webpract.dart';
 
 class Articles extends StatefulWidget {
-  Articles({Key key, this.articleSource, this.articleBody}) : super(key: key);
-  final String articleSource;
-  final String articleBody;
-
+  Articles({Key key, this.name}) : super(key: key);
+  final String name;
   @override
   _ArticlesState createState() => _ArticlesState();
 }
@@ -13,14 +12,11 @@ class _ArticlesState extends State<Articles> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Flutter Virus'),
+      ),
       body: SafeArea(
-        child: ListView(
-          children: <Widget>[
-            Text(widget.articleSource),
-            Text(widget.articleBody),
-          ],
-        ),
+        child: Website(url: widget.name),
       ),
     );
   }
